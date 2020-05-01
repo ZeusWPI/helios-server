ab8aab27698c6f6e897648ca1af61b703d0c1bb6
redfast00 <redfast00@gmail.com>
2020-05-01 05:20:02 +0200

    More settings

--- docker_scripts/start_web.sh
+++ docker_scripts/start_web.sh
@@ -4,6 +4,6 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 python manage.py makemigrations
 python manage.py migrate
 #echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='ben@adida.net', info={'name':'Ben Adida'})" | python manage.py shell
-echo "from helios_auth.models import User; User.update_or_create(user_type='password', user_id=0, name='admin', info={'password': 'password', 'email': 'admin@zeus.ugent.be'})" | python manage.py shell || true
+echo "from helios_auth.models import User; User.update_or_create(user_type='password', user_id='admin', name='admin', info={'password': 'password', 'email': 'admin@zeus.ugent.be'})" | python manage.py shell || true
 
 gunicorn wsgi:application -b 0.0.0.0:8000 -w 8
