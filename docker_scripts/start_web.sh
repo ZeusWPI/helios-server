2a680932c22e29b6c43a0597e4466d22c175fd9a
redfast00 <redfast00@gmail.com>
2020-05-01 19:05:12 +0200

    Add Zeus branding

--- docker_scripts/start_web.sh
+++ docker_scripts/start_web.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e  # Exit immediately if a command exits with a non-zero status.
-
+sleep 5
 python manage.py makemigrations
 python manage.py migrate
-#echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='ben@adida.net', info={'name':'Ben Adida'})" | python manage.py shell
-echo "from helios_auth.models import User; User.update_or_create(user_type='password', user_id='admin', name='admin', info={'password': 'password', 'email': 'admin@zeus.ugent.be'})" | python manage.py shell || true
+
+echo "from helios_auth.models import User; u = User.update_or_create(user_type='password', user_id='admin', name='admin', info={'password': '${ADMIN_PASSWORD}', 'email': '${ADMIN_EMAIL}'}); u.admin_p = True; u.save()" | python manage.py shell || true
 
 gunicorn wsgi:application -b 0.0.0.0:8000 -w 8
